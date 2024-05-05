d0866f60408e5d9032c492a35af2e1901e1a0b62
Shahadat <shobujlingdu@gmail.com>
2024-05-05 22:29:51 +0600

    Download buttons removed

--- resources/js/Pages/Leaves/Index.vue
+++ resources/js/Pages/Leaves/Index.vue
@@ -8,10 +8,10 @@
 
 		<template #right-header>
 			<!-- Downloads -->
-			<download-dropdown class="mr-4">
+			<!-- <download-dropdown class="mr-4">
 				<pdf-download-button :href="route('leaves.pdf', searchQuery)"></pdf-download-button>
 				<excel-download-button :href="route('leaves.excel', searchQuery)"></excel-download-button>
-			</download-dropdown>
+			</download-dropdown> -->
 
 			<!-- Filters -->
 			<filter-dropdown v-model="filters" @reset="reset">
